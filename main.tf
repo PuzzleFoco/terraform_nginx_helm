902f4a8307783214124b561a86448c3914366221
FabiusE <fabius.engel@msg.group>
2020-02-25 15:42:29 +0100

    Changed _ to -

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ data "helm_repository" "nginx-stable" {
 }
 
 resource "helm_release" "nginx_release" {
-    name        = "nginx_ingress"
-    namespace   = "nginx_ingress"
+    name        = "nginx-ingress"
+    namespace   = "nginx-ingress"
     repository  = data.helm_repository.nginx-stable.metadata[0].name
     chart       = "nginx-stable/nginx-ingress"
     timeout     = 600
