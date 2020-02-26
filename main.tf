f22a7b6375cd859a82f721c9ee60bfb4eb72c999
FabiusE <fabius.engel@msg.group>
2020-02-26 10:55:15 +0100

    Changed something with k8s service account

--- main.tf
+++ main.tf
@@ -37,6 +37,7 @@ provider "helm" {
       client_certificate     = var.client_certificate
       client_key             = var.client_key
       cluster_ca_certificate = var.cluster_ca_certificate
+      insecure               = true
   }
 }
 
