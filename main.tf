f6d5f7a3602a0ad29a028e41fe5b642594da15aa
FabiusE <fabius.engel@msg.group>
2020-02-26 08:58:55 +0100

    Changed something with k8s service account

--- main.tf
+++ main.tf
@@ -1,4 +1,35 @@
+resource "kubernetes_service_account" "tiller" {
+  metadata {
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+
+  automount_service_account_token = true
+}
+
+resource "kubernetes_cluster_role_binding" "tiller" {
+  metadata {
+    name = "tiller"
+  }
+
+  role_ref {
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  subject {
+    kind = "ServiceAccount"
+    name = "tiller"
+
+    api_group = ""
+    namespace = "kube-system"
+  }
+}
+
 provider "helm" {
+
+  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
 
     kubernetes {
       host                   = var.host
