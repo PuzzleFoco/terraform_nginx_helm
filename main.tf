28ee2860263f5d3aa945b509370fc5515a6c333c
FabiusE <fabius.engel@msg.group>
2020-02-27 13:24:08 +0100

    added alias

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ data "helm_repository" "nginx-stable" {
 }
 
 resource "helm_release" "nginx_release" {
-    provider    = var.alias
+    provider    = "helm.${var.alias}"
     name        = "nginx-ingress"
     namespace   = kubernetes_namespace.nginx_namespace.metadata[0].name
     repository  = data.helm_repository.nginx-stable.metadata[0].name
