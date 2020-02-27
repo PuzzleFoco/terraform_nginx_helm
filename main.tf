30866795608133e8da82d459db787b38286bc917
FabiusE <fabius.engel@msg.group>
2020-02-27 11:12:19 +0100

    Changed Provider to Var

--- main.tf
+++ main.tf
@@ -12,9 +12,9 @@ provider "helm" {
 
     kubernetes {
       host                   = var.host
-      client_certificate     = var.client_certificate)
-      client_key             = var.client_key)
-      cluster_ca_certificate = var.cluster_ca_certificate)
+      client_certificate     = var.client_certificate
+      client_key             = var.client_key
+      cluster_ca_certificate = var.cluster_ca_certificate
       load_config_file       = false
   }
 }
