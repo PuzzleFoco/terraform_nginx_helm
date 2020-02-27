5410568b413e9f61423612e931217330b2452290
FabiusE <fabius.engel@msg.group>
2020-02-27 11:07:13 +0100

    Added load_config_file       = false

--- example/main.tf
+++ example/main.tf
@@ -6,6 +6,7 @@ provider "helm" {
       client_certificate     = base64decode(module.k8s.client_certificate)
       client_key             = base64decode(module.k8s.client_key)
       cluster_ca_certificate = base64decode(module.k8s.cluster_ca_certificate)
+      load_config_file       = false
   }
 }
 
--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+resource "kubernetes_namespace" "nginx_namespace" {
+    metadata {
+        annotaions = {
+            name = "nginx"
+        }
+        name = "nginx"
+    }
+}
+
+provider "helm" {
+    version        = "~> 1.0.0"
+
+    kubernetes {
+      host                   = module.k8s.host
+      client_certificate     = base64decode(module.k8s.client_certificate)
+      client_key             = base64decode(module.k8s.client_key)
+      cluster_ca_certificate = base64decode(module.k8s.cluster_ca_certificate)
+      load_config_file       = false
+  }
+}
+
 data "helm_repository" "nginx-stable" {
   name = "nginx-stable"
   url  = "https://helm.nginx.com/stable"
@@ -5,7 +26,7 @@ data "helm_repository" "nginx-stable" {
 
 resource "helm_release" "nginx_release" {
     name        = "nginx-ingress"
-    namespace   = "nginx-ingress"
+    namespace   = kubernetes_namespace.nginx_namespace.metadata.name
     repository  = data.helm_repository.nginx-stable.metadata[0].name
     chart       = "nginx-stable/nginx-ingress"
     timeout     = 600
