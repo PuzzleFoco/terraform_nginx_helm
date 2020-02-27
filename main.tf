3b11ceeb906ea808441ed5f410a4025fb31aad89
FabiusE <fabius.engel@msg.group>
2020-02-27 11:11:21 +0100

    Changed Provider to Var

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ provider "helm" {
     version        = "~> 1.0.0"
 
     kubernetes {
-      host                   = module.k8s.host
-      client_certificate     = base64decode(module.k8s.client_certificate)
-      client_key             = base64decode(module.k8s.client_key)
-      cluster_ca_certificate = base64decode(module.k8s.cluster_ca_certificate)
+      host                   = var.host
+      client_certificate     = var.client_certificate)
+      client_key             = var.client_key)
+      cluster_ca_certificate = var.cluster_ca_certificate)
       load_config_file       = false
   }
 }
